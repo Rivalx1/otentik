63fcd2255922e25345bee511219e18c735198b43
Rivalx1 <70897802+Rivalx1@users.noreply.github.com>
2025-06-07 07:01:10 +0700

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,16 +1,16 @@
 export async function onRequestGet(context) {
   const url = new URL(context.request.url);
   const id = url.searchParams.get("id");
-  const token = url.searchParams.get("auth");
+  // const token = url.searchParams.get("auth");
 
   const CHANNELS = {
     "channel1": "https://asli.server.com/stream1.m3u8",
     "channel2": "https://example.tv/live2/index.m3u8",
   };
 
-  if (token !== "rahasia123") {
-    return new Response("Unauthorized", { status: 403 });
-  }
+  // if (token !== "rahasia123") {
+  //   return new Response("Unauthorized", { status: 403 });
+  // }
 
   if (!id || !CHANNELS[id]) {
     return new Response("Channel not found", { status: 404 });
